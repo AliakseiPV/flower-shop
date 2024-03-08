3a0d918d10182bafc9c34cd2f7997fbeae7fac79
Aliaksei Poliak <aliakseipv@gmail.com>
2024-03-07 21:20:43 -0500

    refactor admin form component

--- src/app/(adminRotes)/products/page.tsx
+++ src/app/(adminRotes)/products/page.tsx
@@ -1,21 +1,58 @@
-import { FC } from 'react'
-import { AdminForm, AdminProduct } from '@/components'
+import { addProduct } from '@/actions/productActions'
+import { AdminForm, AdminProduct, Modal } from '@/components'
 import { prisma } from '@/prisma'
+import Link from 'next/link'
 
-const ProductsPage: FC = async () => {
+const ProductsPage = async ({
+	searchParams }: {
+		searchParams: { [key: string]: string | string[] | undefined }
+	}) => {
+
+	const modal = searchParams.modal
+	const productId = Number(searchParams.id)
 
 	const products = await prisma.product.findMany()
 
+	let product = null
+	if (productId) {
+		product = await prisma.product.findUnique({
+			where: {
+				id: productId
+			}
+		})
+	}
+
 	return (
 		<div>
-			<AdminForm />
+
+			<Link href={`?modal=POST`}>
+				Add new Product
+			</Link>
+
+			{modal === 'POST' &&
+				<Modal searchParams={{ modal }}>
+					<AdminForm
+						productAction={addProduct}
+						successMessage={'Product added'}
+						product={null}
+					/>
+				</Modal>
+			}
+			{modal === 'PUT' &&
+				<Modal searchParams={{ modal }}>
+					<AdminForm
+						productAction={addProduct}
+						successMessage={'Product updated'}
+						product={product}
+					/>
+				</Modal>
+			}
 
 			<div>
 				{products.map((product) => (
-					<AdminProduct product={product} />		
+					<AdminProduct key={product.id} product={product} />
 				))}
 			</div>
-
 
 		</div>
 	)
--- src/components/adminForm/adminForm.tsx
+++ src/components/adminForm/adminForm.tsx
@@ -1,47 +1,84 @@
 "use client"
 
-import React, { useRef } from 'react'
-import { addProduct } from '@/actions/productActions'
 import { InputImage } from '../inputImage'
 import { toast } from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
+import type { productType } from '@/types/productType'
 
-const AdminForm = () => {
 
-	const ref = useRef<HTMLFormElement>(null)
+const AdminForm = async ({
+	productAction,
+	successMessage,
+	product,
+}: {
+	productAction: Function,
+	successMessage: string,
+	product: productType | null
+}
+) => {
+
+	const router = useRouter()
+
+	const formAction = async (formData: FormData) => {
+		router.back()
+		const result = await productAction(formData)
+		if (result?.error) {
+			toast.error(result.error)
+		} else {
+			toast.success(successMessage)
+		}
+	}
 
 	return (
+
 		<form
-			ref={ref}
-			action={async (formData) => {
-				ref.current?.reset()
-				const result = await addProduct(formData)
-				if (result?.error) {
-					toast.error(result.error)
-				} else {
-					toast.success("Product added")
-				}
-			}}
+			action={formAction}
 		>
 			<div>
-				<InputImage inputName={'productImage'} imgWidth={250} imgHeight={300} />
+				<div>
+					<InputImage
+						imgSrc={product?.img[0] ? `/images/${product.img[0]}` : ''}
+						inputName={'productImage'}
+						imgWidth={250}
+						imgHeight={300}
+					/>
+				</div>
+				<div>
+					<div>
+						<InputImage
+							imgSrc={product?.img[1] ? `/images/${product.img[1]}` : ''}
+							inputName={'productImage'}
+							imgWidth={150}
+							imgHeight={200} />
+						<InputImage
+							imgSrc={product?.img[2] ? `/images/${product.img[2]}` : ''}
+							inputName={'productImage'}
+							imgWidth={150}
+							imgHeight={200} />
+						<InputImage
+							imgSrc={product?.img[3] ? `/images/${product.img[3]}` : ''}
+							inputName={'productImage'}
+							imgWidth={150}
+							imgHeight={200} />
+					</div>
+					<div>
+						<input defaultValue={product?.title || ''} type="text" name='title' placeholder='Name...' required />
+						<input defaultValue={product?.details || ''} type="text" name='details' placeholder='Short description...' required />
+						<input defaultValue={product?.description || ''} type="text" name='description' placeholder='Description...' />
+						<input defaultValue={product?.price || ''} type="number" step="0.01" name='price' placeholder='Price' />
+					</div>
+					<div>
+						{product?.Availability ?
+							<input type="checkbox" name="availability" value="true" defaultChecked />
+							:
+							<input type="checkbox" name="availability" value="true" />
+						}
+
+						<button>Submit</button>
+					</div>
+				</div>
 			</div>
-			<div>
-				<div>
-					<InputImage inputName={'productImage'} imgWidth={150} imgHeight={200} />
-					<InputImage inputName={'productImage'} imgWidth={150} imgHeight={200} />
-					<InputImage inputName={'productImage'} imgWidth={150} imgHeight={200} />
-				</div>
-				<div>
-					<input type="text" name='title' placeholder='Name...' required />
-					<input type="text" name='details' placeholder='Short description...' required />
-					<input type="text" name='description' placeholder='Description...' />
-					<input type="number" step="0.01" name='price' placeholder='Price' />
-				</div>
-				<div>
-					<input type="checkbox" name="availability" value="true" />
-					<button>Submit</button>
-				</div>
-			</div>
+
 		</form>
 	)
 }

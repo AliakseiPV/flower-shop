f2e56d30838bd01cb8f023394a71d22cea1e3556
Aliaksei Poliak <aliakseipv@gmail.com>
2024-03-07 21:19:02 -0500

    refactor input image component

--- src/components/inputImage/inputImage.tsx
+++ src/components/inputImage/inputImage.tsx
@@ -2,42 +2,23 @@
 
 import { ChangeEvent, useState } from 'react'
 import Image from 'next/image'
-import { useFormStatus } from 'react-dom'
 
 type inputImageType = {
+	imgSrc: string,
 	inputName: string,
 	imgWidth: number,
 	imgHeight: number,
 }
 
 const InputImage = (props: inputImageType) => {
-	const { inputName, imgWidth, imgHeight } = props
+	const { inputName, imgWidth, imgHeight, imgSrc } = props
 
-	const { pending } = useFormStatus()
-	const [image, setImage] = useState('')
+	const [image, setImage] = useState(imgSrc)
 
 	const changeInputImgHandler = ({ target }: ChangeEvent<HTMLInputElement>) => {
 		if (target.files) {
 			const file = target.files[0]
 			setImage(URL.createObjectURL(file))
-		}
-	}
-
-	const showImage = () => {
-		if (!pending && !image ) {
-			return (<span>Add Image</span>)
-		}
-		if (!pending && image) {
-			return (<Image
-				src={image}
-				alt={'product-image'}
-				width={imgWidth}
-				height={imgHeight}
-			/>)
-		}
-		if (pending && image) {
-			setImage('')
-			return (<span>Add Image</span>)
 		}
 	}
 
@@ -51,7 +32,16 @@ const InputImage = (props: inputImageType) => {
 				hidden
 			/>
 			<div>
-				{showImage()}
+				{image ?
+					<Image
+						src={image}
+						alt={'product-image'}
+						width={imgWidth}
+						height={imgHeight}
+					/>
+					:
+					<span>Add Image</span>
+				}
 			</div>
 		</label>
 	)

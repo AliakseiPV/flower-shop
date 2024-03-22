6a001d253599ecbed77b9670a96964611de7c867
Aliaksei Poliak <aliakseipv@gmail.com>
2024-03-22 17:58:02 -0400

    refactor sort function

--- src/utiles/sortProducts.ts
+++ src/utiles/sortProducts.ts
@@ -11,24 +11,36 @@ export const filterProductByParams = (products: Array<productType>, filterParams
 	}
 
 	if (filterParams === 'createdAt') {
-		productArr.array.sort((a, b) => Number(a.createdAt) - Number(b.createdAt))
-		return productArr
-	} else if (filterParams === 'bouquet' || filterParams === 'flower') {
-		productArr.array = productArr.array.filter((product) => product.type === filterParams)
-		return productArr
-	} else {
-		const array = productArr.array.filter((product) =>
-			product.title.toLowerCase().includes(filterParams) ||
-			product.details.toLowerCase().includes(filterParams))
-
-		if (!array.length) {
-			productArr.array.sort((a, b) => Number(a.createdAt) - Number(b.createdAt))
-			productArr.error = 'No results found'
-			return productArr
-		}
-
-		productArr.array = array
-		
+		productArr.array.sort((a, b) => Number(b.createdAt) - Number(a.createdAt))
 		return productArr
 	}
+
+	if (filterParams === 'bouquet' || filterParams === 'flower') {
+		productArr.array = productArr.array.filter((product) => product.type === filterParams)
+		return productArr
+	}
+
+	if (filterParams === 'available') {
+		productArr.array = productArr.array.filter((product) => product.availability)
+		return productArr
+	}
+
+	if (filterParams === 'notAvailable') {
+		productArr.array = productArr.array.filter((product) => !product.availability)
+		return productArr
+	}
+
+	const array = productArr.array.filter((product) =>
+		product.title.toLowerCase().includes(filterParams) ||
+		product.details.toLowerCase().includes(filterParams))
+
+	if (!array.length) {
+		productArr.array.sort((a, b) => Number(b.createdAt) - Number(a.createdAt))
+		productArr.error = 'No results found'
+		return productArr
+	}
+
+	productArr.array = array
+
+	return productArr
 }

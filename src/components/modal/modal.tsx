e583d1acb80d1c9fd43d467e9f01ef3db1acf0ce
Aliaksei Poliak <aliakseipv@gmail.com>
2024-03-22 17:59:59 -0400

    fix modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,18 +1,21 @@
-'use client'
+"use client"
 
 import { ReactNode } from "react"
 import styles from './modal.module.css'
-import { useRouter } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faX } from '@fortawesome/free-solid-svg-icons'
+
 
 const Modal = ({
 	children,
-	modalParams,
 }: {
 	children: ReactNode | undefined,
-	modalParams: string | undefined
 }) => {
 
 	const router = useRouter()
+	const params = useSearchParams()
+	const modalParams = params.get('modal')
 
 	return (
 		<div className={styles.modal} style={
@@ -27,7 +30,7 @@ const Modal = ({
 					onClick={router.back}
 					className={styles.close}
 				>
-					Close
+					<FontAwesomeIcon icon={faX} size="2xl" />
 				</button>
 				{children}
 			</div>

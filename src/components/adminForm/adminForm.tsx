1a4237bde4be94cd40154689abb8bdf55590c63c
Aliaksei Poliak <aliakseipv@gmail.com>
2024-03-22 18:00:36 -0400

    add admin form styles

--- src/components/adminForm/adminForm.tsx
+++ src/components/adminForm/adminForm.tsx
@@ -4,6 +4,7 @@ import { InputImage } from '../inputImage'
 import { toast } from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
 import type { productType } from '@/types/productType'
+import styles from './adminForm.module.css'
 
 const AdminForm = ({
 	productAction,
@@ -32,52 +33,97 @@ const AdminForm = ({
 
 		<form
 			action={formAction}
+			className={styles.form}
 		>
-			<div>
-				<div>
+			<div className={styles.content}>
+				<div className={styles['main-img']}>
 					<InputImage
 						imgSrc={product?.img[0].length ? `/images/${product.img[0]}` : ''}
 						inputName={'Image0'}
 						imgWidth={250}
-						imgHeight={300}
+						imgHeight={320}
 					/>
 				</div>
-				<div>
-					<div>
+				<div >
+					<div className={styles['img-container']}>
 						<InputImage
 							imgSrc={product?.img[1].length ? `/images/${product.img[1]}` : ''}
 							inputName={'Image1'}
-							imgWidth={150}
-							imgHeight={200} />
+							imgWidth={170}
+							imgHeight={200}
+						/>
 						<InputImage
 							imgSrc={product?.img[2].length ? `/images/${product.img[2]}` : ''}
 							inputName={'Image2'}
-							imgWidth={150}
-							imgHeight={200} />
+							imgWidth={170}
+							imgHeight={200}
+						/>
 						<InputImage
 							imgSrc={product?.img[3].length ? `/images/${product.img[3]}` : ''}
 							inputName={'Image3'}
-							imgWidth={150}
-							imgHeight={200} />
+							imgWidth={170}
+							imgHeight={200}
+						/>
 					</div>
-					<div>
-						<input defaultValue={product?.title || ''} type="text" name='title' placeholder='Name...' required />
-						<input defaultValue={product?.details || ''} type="text" name='details' placeholder='Short description...' required />
-						<input defaultValue={product?.description || ''} type="text" name='description' placeholder='Description...' />
-						<input defaultValue={product?.price || ''} type="number" step="0.01" name='price' placeholder='Price' />
-						<select name="type">
+					<div className={styles['content-wrapper']}>
+						<input
+							defaultValue={product?.title || ''}
+							type="text"
+							name='title'
+							placeholder='Name...'
+							required
+							className={styles['input-text']}
+						/>
+						<input
+							defaultValue={product?.details || ''}
+							type="text"
+							name='details'
+							placeholder='Short description...'
+							required
+							className={styles['input-text']}
+						/>
+						<textarea
+							defaultValue={product?.description || ''}
+							name='description'
+							placeholder='Description...'
+							className={styles.description}
+						/>
+						<input
+							defaultValue={product?.price || ''}
+							type="number"
+							step="0.01"
+							name='price'
+							placeholder='Price'
+							className={styles['input-text']}
+						/>
+					</div>
+					<div className={styles.footer}>
+
+						<select className={styles.select} name="type">
 							<option value="flower">Flower</option>
 							<option value="bouquet">Bouquet</option>
 						</select>
-					</div>
-					<div>
+
 						{product?.availability ?
-							<input type="checkbox" name="availability" value="true" defaultChecked />
+
+							<input
+								className={styles.availability}
+								type="checkbox" name="availability"
+								value="true"
+								defaultChecked
+							/>
+
 							:
-							<input type="checkbox" name="availability" value="true" />
+							<input
+								className={styles.availability}
+								type="checkbox"
+								name="availability"
+								value="true"
+							/>
+
 						}
 
-						<button>Submit</button>
+						<button className={styles.btn}>Submit</button>
 					</div>
 				</div>
 			</div>

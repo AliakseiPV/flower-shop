8c33458f7081a518ab080ecfc91b1361e4402e67
Aliaksei Poliak <aliakseipv@gmail.com>
2024-04-03 13:51:31 -0400

    add get cartFormData func

--- src/utiles/getFormData.ts
+++ src/utiles/getFormData.ts
@@ -1,9 +1,9 @@
-export const getFormData = (formData: FormData) => {
+export const getAdminFormData = (formData: FormData) => {
 	const data: {
 		title: string,
 		details: string,
 		description: string | null,
-		price: number | null,
+		price: number,
 		availability: boolean,
 		type: string
 	} = {
@@ -14,6 +14,20 @@ export const getFormData = (formData: FormData) => {
 		availability: Boolean(formData.get('availability')) || false,
 		type: formData.get('type') as string
 	}
-	
+
+	return data
+}
+
+export const getCartFormData = (FormData: FormData) => {
+	const data: {
+		name: string,
+		phone: string,
+		email: string
+	} = {
+		name: FormData.get('name') as string,
+		phone: FormData.get('phone') as string,
+		email: FormData.get('email') as string
+	}
+
 	return data
 }

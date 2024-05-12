3907156e078db7a960607a21af7f07827b99cb48
Aliaksei Poliak <aliakseipv@gmail.com>
2024-05-12 15:42:57 -0400

    add loading

---
+++ src/app/(routes)/catalog/loading.tsx
@@ -0,0 +1,7 @@
+import { Spinner } from "@nextui-org/react";
+
+export default function Loading() {
+	return (
+		<Spinner size="lg" color="secondary" className="w-full justify-center items-center"/>
+	)
+}

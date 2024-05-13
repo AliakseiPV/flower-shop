be702c92990cf8c4cb87c12f058042b61ce6e6bb
Aliaksei Poliak <aliakseipv@gmail.com>
2024-05-13 11:04:03 -0400

    prisma delene user model

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,19 +3,7 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
-	const user = await prisma.user.upsert({
-		where: {
-			email: 'test@test.com'
-		},
-		update: {},
-		create: {
-			name: 'Admin',
-			email: 'test@test.com',
-			password: 'password',
-			isAdmin: true
-		}
-	})
-	console.log(user)
+
 }
 
 main()

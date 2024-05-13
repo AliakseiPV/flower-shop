7d6fd81da7784e3302c07fd06c15ff62e037ab3a
Aliaksei Poliak <aliakseipv@gmail.com>
2024-05-13 11:33:03 -0400

    refactoring cart list component

--- src/components/cartList/cartList.tsx
+++ src/components/cartList/cartList.tsx
@@ -2,11 +2,11 @@
 
 import { Context } from "@/context/cartContext"
 import { useContext } from "react"
-import { checkoutAction } from "@/actions/checkoutAction"
-import toast from "react-hot-toast"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faMinus, faEraser } from '@fortawesome/free-solid-svg-icons'
+import { faMinus, faEraser, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { ActionButton } from "../actionButton"
+import { Card, CardBody, CardHeader, Image } from "@nextui-org/react"
+import { findExistingImg } from "@/utiles/findExistingImg"
 
 const CartList = () => {
 
@@ -18,23 +18,45 @@ const CartList = () => {
 
 				<div key={product.data.id}>
 
-					<ActionButton
-						clickAction={() => handleRemoveAllFromCart(product.data)}
-						className="bg-red-500 font-semibold"
-						successMessage={`${product.data.title} sucsessfully deleted`}>
-						<FontAwesomeIcon icon={faEraser} />
-						Remove
-					</ActionButton>
 
-					<h2>{product.data.title}</h2>
-					<span>Quantity{product.quantity}</span>
+					<Card className="py-4 relative max-w-[250px]">
+						<CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
 
-					<ActionButton
-						clickAction={() => handleRemoveOneFromCart(product.data)}
-						className="bg-red-500 font-semibold rounded-full"
-						successMessage={`${product.data.title} sucsessfully deleted`}>
-						<FontAwesomeIcon icon={faMinus} />
-					</ActionButton>
+							<ActionButton
+								isIconOnly={true}
+								isDisabled={false}
+								clickAction={() => handleRemoveAllFromCart(product.data)}
+								className="font-semibold text-red-700 absolute right-4 top-4"
+								successMessage={`${product.data.title} sucsessfully deleted`}
+							>
+								<FontAwesomeIcon icon={faTrash} />
+							</ActionButton>
+
+							<h2 className="text-tiny uppercase font-bold">{product.data.title}</h2>
+							<small className="text-default-500">{product.data.details}</small>
+
+						</CardHeader>
+						<CardBody className="overflow-visible py-2">
+							<Image
+								alt="Card background"
+								className="object-cover rounded-xl w-40 h-52"
+								src={`${process.env.NEXT_PUBLIC_AWS_S3_OBJECT_URL}/productsImages/${findExistingImg(product.data).name}`}
+							/>
+							<div className="font-bold text-large flex gap-2  items-center">
+								<span>quantity:</span>
+								<span >{product.quantity}</span>
+								<ActionButton
+									isDisabled={false}
+									isIconOnly={true}
+									clickAction={() => handleRemoveOneFromCart(product.data)}
+									className="text-red-700 font-semibold text-xl"
+									successMessage={`${product.data.title} sucsessfully deleted`}>
+									<FontAwesomeIcon icon={faMinus} />
+								</ActionButton>
+							</div>
+						</CardBody>
+					</Card>
+
 				</div>
 
 			))}
